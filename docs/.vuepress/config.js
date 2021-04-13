27b40ef58a6c63ce8272cf52708a8b0d6ccdb347
aBian0609 <glareboy@outlook.com>
2021-04-13 13:40:07 +0800

    添加部署逻辑代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,22 +9,10 @@ module.exports = {
   ],
   dest: './docs/.vuepress/dist',
   ga: '',
+  base: '/blog/',
   evergreen: true,
   themeConfig: {
     logo: '/images/logo.png',
-    sidebar: [
-      {
-        title: 'Guide',   // 必要的
-        path: '/guide/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 可选的, 默认值是 true,
-        sidebarDepth: 1,    // 可选的, 默认值是 1
-        children: [
-          '/',
-          '/BOM',
-          '/DOM'
-        ]
-      },
-    ],
     nav: [{
         text: 'Home',
         link: '/'
